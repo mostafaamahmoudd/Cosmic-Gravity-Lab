6417706c429c1c8fe62b508253f55b7b9e352aed
Mostafa <mostafamahmoud805554@std.mans.edu.eg>
2023-11-28 13:46:10 +0200

    Project Clone
    احلي مسا يا ابو علاء

---
+++ Cosmic Gravity Lab/Assets/SCRIPTS/score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoretext;
+    int score_ = 0;
+
+    void Update()
+    {
+        scoretext.text = score_.ToString();
+        ++score_;
+        //scoretext.text = player.position.z.ToString("0");
+    }
+}
